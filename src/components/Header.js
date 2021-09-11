b4f09f5f3e309012c6476c4fc3dab482c6951fa4
Humza Sajid <pak786hamzasajid66@Gmail.com>
2021-09-11 09:46:21 +0500

    2nd update

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,10 +2,7 @@ import React from 'react';
 import Order from './Order';
 function Header(props)
 {
-
-  
     return(
-
         <nav className="navbar  navbar-dark bg-dark">
         <div className="container-fluid">
           <span className="navbar-brand mb-0 h1 text-center " style={{textAlign: 'center', position: 'relative', left: '40%'}}>
